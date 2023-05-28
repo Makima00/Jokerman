f292140287442064f0b4529604da2def7922f825
Makima00 <makimam492@gmail.com>
2023-05-28 14:45:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,4 +13,16 @@ bot.on('message', (payload, chat) => {
     chat.say(`Echo: ${text}`);
 });
 
+bot.on('message', (payload, chat) => {
+    console.log('A text message was received!');
+});
+
+bot.on('attachment', (payload, chat) => {
+    console.log('An attachment was received!');
+});
+
+bot.on('postback:HELP_ME', (payload, chat) => {
+    console.log('The Help Me button was clicked!');
+});
+
 bot.start();
