8d1c77dedd02e7cfb4b1d46db7ee2114b1bcd9e6
Makima00 <makimam492@gmail.com>
2023-05-28 15:47:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,12 @@ const bot = new BootBot({
     appSecret: process.env.appSecret
 });
 
-bot.on('message', (payload, chat) => {
-    const text = payload.message.text;
-
-    chat.say(`Vous avez ecris: "${text}"`);
-});
 
 // Meeting
 bot.hear(['Salut', 'Bonjour', 'hi', 'slt', /Kaiz (Kaiz)?/i], (payload, chat) => {
     chat.getUserProfile().then((user) => {
         chat.say(`Bonjour, ${user.first_name}!`).then(() => {
-            chat.say('Que puis-je faire pour toi?')
+            chat.say('Que puis-je faire pour toi?', { typing: true })
         });
     });
 });
